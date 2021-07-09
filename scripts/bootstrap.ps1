500562255abb1ffa5f831b0b93fd46095bfcf581
Serg Nikolaev <kndnou@yandex.ru>
2021-07-10 00:02:21 +1100

    add user model

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -1,1 +1,3 @@
 pip install -r requirements.txt
+$env:FLASK_APP="webapp.server"
+flask db upgrade
--- webapp/server.py
+++ webapp/server.py
@@ -1,8 +1,8 @@
 from flask import Flask
 # from flask_login import LoginManager
-# from flask_migrate import Migrate
-# from webapp.db import db
-# from webapp.user.models import User
+from flask_migrate import Migrate
+from webapp.db import db
+from webapp.user.models import User
 from webapp.clip.views import blueprint as clip_blueprint
 from webapp.user.views import blueprint as user_blueprint
 
@@ -10,8 +10,8 @@ from webapp.user.views import blueprint as user_blueprint
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
-    # db.init_app(app)
-    # migrate = Migrate(app, db)
+    db.init_app(app)
+    Migrate(app, db, render_as_batch=True)
 
     # login_manager = LoginManager()
     # login_manager.init_app(app)
