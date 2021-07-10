4b62e1bc1a6b2c9a2c48fbb897385c50dc199eaf
Serg Nikolaev <kndnou@yandex.ru>
2021-07-10 23:50:11 +1100

    add admin to constant variable and modify users model attributes

--- webapp/user/models.py
+++ webapp/user/models.py
@@ -3,13 +3,15 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from webapp.db import db
 
+ADMIN_ROLES = ['admin']
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    password = db.Column(db.String(128))
-    role = db.Column(db.String(10), index=True)
-    email = db.Column(db.String(50), unique=True)
+    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    role = db.Column(db.String(10), index=True, nullable=True)
+    email = db.Column(db.String(50), unique=True, nullable=False)
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -19,7 +21,7 @@ class User(db.Model, UserMixin):
 
     @property
     def is_admin(self):
-        return self.role == 'admin'
+        return self.role in ADMIN_ROLES
 
     def __repr__(self):
         return '<User name={} id={}>'.format(self.username, self.id)
